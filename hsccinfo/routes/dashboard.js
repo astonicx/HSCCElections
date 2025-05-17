343b16437ccd5a46bdca4858227dad38f6e420b9
astonicx <120275795+astonicx@users.noreply.github.com>
2025-05-17 16:42:20 +0000

    dahsbord conection

--- hsccinfo/routes/dashboard.js
+++ hsccinfo/routes/dashboard.js
@@ -8,7 +8,14 @@ router.get('/', auth, function(req, res, next) {
   if (process.env.CONSOLE_DEBUG=="true"){
     console.log("Dashboard");
   }
-  res.render('dashboard', { title: 'Dashboard',username: res.locals.name, role: res.locals.role });
-});
+  if (!res.locals.role|| res.locals.role=="guest"){
+    res.redirect('/logintest');
+    res.render('logintest', { title: 'Please log in',message:'You need to log in to access page' });
+  }
+  else{ 
+    res.render('dashboard', { title: 'Dashboard', username:res.locals.name, roles:res.locals.roles, userToken: res.locals.userToken });
+  }
+}
+);
 //Gratuitous comment
 module.exports = router;
